1a328f335edbe1f5c9d2501446f92edad55ebf2d
zhuhaibo <zhuhaibo@niwodai.net>
2018-03-09 10:52:26 +0800

    Initial Commit

---
+++ Hello Av Foundation/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Hello Av Foundation
+//
+//  Created by tom on 09/03/2018.
+//  Copyright © 2018 TZ. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Hello Av Foundation/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Hello Av Foundation
+//
+//  Created by tom on 09/03/2018.
+//  Copyright © 2018 TZ. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
